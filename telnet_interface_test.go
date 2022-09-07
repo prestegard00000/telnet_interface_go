765b8ef8221a099c2de04fc9c151ac0cc4f0594a
Aaron Prestegard <prestegard00000@gmail.com>
2022-09-07 04:11:57 -0700

    Capitalized Telnet_connect() function to match changes to telnet_interface.go modual

--- telnet_interface_test.go
+++ telnet_interface_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 // Test for empty sites
-func test_telnet_connect(t *testing.T) {
+func test_Telnet_connect(t *testing.T) {
 	test_site := ""
 	test_portNumber := 5555
 	expected_string := "welcome"
 	expected_result := regexp.MustCompile(expected_string)
-	msg, err := telnet_connect(test_site, test_portNumber)
+	msg, err := Telnet_connect(test_site, test_portNumber)
 	if !expected_result.MatchString(msg) || err != nil {
-		t.Fatalf("telnet_connect(%s, %d) = %q, nil", test_site, test_portNumber, msg)
+		t.Fatalf("Telnet_connect(%s, %d) = %q, nil", test_site, test_portNumber, msg)
 	}
 }

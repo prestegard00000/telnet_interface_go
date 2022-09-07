e2f27d0daa75910ef2354966783e1934ccaa4254
Aaron Prestegard <prestegard00000@gmail.com>
2022-09-07 04:09:17 -0700

    Capitalized function Telnet_connect, so it would be externalized

--- telnet_interface.go
+++ telnet_interface.go
@@ -2,12 +2,12 @@ package communications
 
 import (
 	"errors"
-  	"fmt"
+	"fmt"
 
 	"github.com/reiver/go-telnet"
 )
 
-func telnet_connect(site string, portNumber int) (string, error) {
+func Telnet_connect(site string, portNumber int) (string, error) {
 	var caller telnet.Caller = telnet.StandardCaller
 
 	if site == "" {
@@ -22,4 +22,14 @@ func telnet_connect(site string, portNumber int) (string, error) {
 	//@TODO: replace "example.net:5555" with address you want to connect to.
 	telnet.DialToAndCall(address, caller)
 	return "hello", nil
+	//return connected if no error
+	//return failed connected
 }
+
+// if connected, send the string
+
+// if connected, get string
+
+// end connection
+
+// test connection

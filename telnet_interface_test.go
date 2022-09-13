21c3fe4d0df7acc03ae57e8e9d0b0812e7044761
Aaron Prestegard <prestegard00000@gmail.com>
2022-09-13 03:17:48 -0700

    feature: added struct to match updates

--- telnet_interface_test.go
+++ telnet_interface_test.go
@@ -7,11 +7,12 @@ import (
 
 // Test for empty sites
 func test_Telnet_connect(t *testing.T) {
+	var testConnection telnet_profile
 	test_site := ""
 	test_portNumber := 5555
 	expected_string := "welcome"
 	expected_result := regexp.MustCompile(expected_string)
-	msg, err := Telnet_connect(test_site, test_portNumber)
+	msg, err := testConnection.Telnet_connect(test_site, test_portNumber)
 	if !expected_result.MatchString(msg) || err != nil {
 		t.Fatalf("Telnet_connect(%s, %d) = %q, nil", test_site, test_portNumber, msg)
 	}

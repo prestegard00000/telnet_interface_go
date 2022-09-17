ed820af1cc2a0e44f084a6ac57dc86b5f58bf6c0
Aaron Prestegard <prestegard00000@gmail.com>
2022-09-16 22:04:33 -0700

    Research: the telnet session opens up and dials correctly, but does not feed into a string or recieve commands via automation without throwing a mememory error.  going to clean up methods to community standards and add proper tests.  Perhaps the answer will be there.

--- telnet_interface.go
+++ telnet_interface.go
@@ -4,6 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"github.com/reiver/go-telnet"
+	"bufio"
+	//"io/ioutil"
+	//"net"
+	//"net/http"
+	//"os"
 )
 
 type Telnet_profile struct {
@@ -13,13 +18,36 @@ type Telnet_profile struct {
 	TransmitionError error
 	Connected        bool
 	Connect		 *telnet.Conn
+	Context		telnet.Context
 	//TransmitConn	telnet.Conn                                                                                                                                                                   	
 	TransmitTo	telnet.Writer
 	TransmitFrom	telnet.Reader
 	TransmitContext telnet.Context
+	TClient		telnet.Client
 }
 
 func (s Telnet_profile) Telnet_connect()(string,error) { //site string, portNumber int) (string, error)
+	address := fmt.Sprintf("%s:%d", s.Site,s.PortNumber)
+	var connection *telnet.Conn
+	connection, s.TransmitionError = telnet.DialTo(address)
+	s.TransmitContext = telnet.NewContext()
+	//s.Connection.CallTELNET(s.TransmitContext, s.TransmitTo, s.TransmitFrom)
+	//s.TransmitionError = s.Connection.Call(connection)
+	s.TransmitionError = s.TClient.Call(connection)
+	
+	//type Client -> Caller ->Logger
+	//error Client.Call(*Conn)
+	//type *Conn (struct)
+	//*Conn, error telnet.DialTo(addr string)
+	//type Caller interface .CallTELNET(Context, Writer, Reader)	
+	//type Context ->Logger
+	//Contex NewContext()
+	
+	//s.TransmitTo.Write(b []byte = "x\n")
+	message, _ := bufio.NewReader(connection).ReadString('\n')
+	fmt.Print("Message from server: " + message)
+
+
 	//@ToDo: change this method to setupProfile and split out the actual calling method
 	//@ToDo: clean up
 	//create a new terminal
@@ -35,13 +63,16 @@ func (s Telnet_profile) Telnet_connect()(string,error) { //site string, portNumb
 	if s.PortNumber == 0 {
 		s.PortNumber = 5555
 	}
-
-	address := fmt.Sprintf("%s:%d", s.Site, s.PortNumber)
+/*
+	//address := fmt.Sprintf("%s:%d", s.Site, s.PortNumber)
 	s.Connect, s.TransmitionError = telnet.DialTo(address) // s.Connection)
-	//fmt.Println("msg: ", msg)
+	//s.Connect.CallTELNET(s.TransmitContext, s.TransmitTo, s.TransmitFrom)
+	var msg string
+	//telnet.Call()
+	msg, s.TransmitionError = s.Telnet_recieveString()
+	fmt.Print(msg)*/
 	//@TODO: return the message
 	return address, s.TransmitionError
-	//@TODO:return connected if no error
 }
 
 func (s Telnet_profile) Telnet_transmitString(transmition string) (string, error) {
@@ -56,15 +87,15 @@ func (s Telnet_profile) Telnet_transmitString(transmition string) (string, error
 
 func (s Telnet_profile) Telnet_recieveString() (string, error) {
 
+	var incomingTransmition string = ""
 	var p []byte
 	var count int
-	var incomingTransmition string = ""
 	count, s.TransmitionError = s.Connect.Read(p)
+	
 	var transmitionError error = nil
 	fmt.Print(count)
 	fmt.Print(p)
 	return incomingTransmition, transmitionError
-	//if telnet.Caller is not nil
 	//recieveString
 	//if the string does not error, return it
 }
